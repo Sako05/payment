70de62207241153ff2f4df6fe5c5a2d461577d16
Sako05 <salem-k@hotmail.com>
2021-09-19 13:04:33 +0200

    Possible to POST method with HTTP request object

--- src/main/java/se/nackademin/java20/pgw/persistance/RabbitNotificationService.java
+++ src/main/java/se/nackademin/java20/pgw/persistance/RabbitNotificationService.java
@@ -5,8 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import se.nackademin.java20.pgw.domain.Payment;
 import se.nackademin.java20.pgw.domain.PaymentNotificationService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RabbitNotificationService implements PaymentNotificationService {
     private final static Logger LOG = LoggerFactory.getLogger(RabbitNotificationService.class);
@@ -29,5 +39,26 @@ public class RabbitNotificationService implements PaymentNotificationService {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://hakimlivsdb.herokuapp.com/payment/add/reference/status";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("reference", payment.getReference());
+        map.add("status", payment.getStatus());
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class );
+
+        LOG.info(map.toString());
+        LOG.info(String.valueOf(response.getStatusCode()));
+        LOG.info(response.getBody());
+
+
     }
 }
