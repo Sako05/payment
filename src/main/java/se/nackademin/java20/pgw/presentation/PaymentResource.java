34793134eb1be215aff55b85d4e1c7258515effe
Sako05 <salem-k@hotmail.com>
2021-09-19 17:47:23 +0200

    test changes

--- src/main/java/se/nackademin/java20/pgw/presentation/PaymentResource.java
+++ src/main/java/se/nackademin/java20/pgw/presentation/PaymentResource.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import se.nackademin.java20.pgw.application.PaymentService;
 
 @Controller
@@ -18,7 +19,7 @@ public class PaymentResource {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<Void> createPayment(@RequestBody PaymentDto paymentDto) {
+    public ResponseEntity<Void> createPayment(@RequestParam PaymentDto paymentDto) {
         paymentService.createPayment(paymentDto.getReference());
         return ResponseEntity.noContent().build();
     }
