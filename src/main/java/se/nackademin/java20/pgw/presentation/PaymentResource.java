b8e94d9bc325b1f27346ce9bcd1831f085d2f8b0
Sako05 <salem-k@hotmail.com>
2021-09-19 18:04:14 +0200

    test changes

--- src/main/java/se/nackademin/java20/pgw/presentation/PaymentResource.java
+++ src/main/java/se/nackademin/java20/pgw/presentation/PaymentResource.java
@@ -2,11 +2,13 @@ package se.nackademin.java20.pgw.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import se.nackademin.java20.pgw.application.PaymentService;
 
 @Controller
+@CrossOrigin
 public class PaymentResource {
 
     private final PaymentService paymentService;
